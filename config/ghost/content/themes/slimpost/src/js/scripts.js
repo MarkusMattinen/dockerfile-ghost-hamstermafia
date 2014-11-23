9a8169000a8c1704e43419e4436a7e584793afff
Markus Mattinen <markus.mattinen@vincit.fi>
2014-11-23 17:54:43 +0200

    Do not wrap images in <a> if already has a link

--- config/ghost/content/themes/slimpost/src/js/scripts.js
+++ config/ghost/content/themes/slimpost/src/js/scripts.js
@@ -1,25 +1,29 @@
 $(document).ready(function() {
-	// FitVids
-	$('.post-excerpt').fitVids();	
+        // FitVids
+        $('.post-excerpt').fitVids();
 
-	// Darkbox
-	$('.post-content img').each(function() {
-		var imageSrc = $(this).attr('src');
+        // Darkbox
+        $('.post-content img').each(function() {
+                if ($this.parent().get(0).tagName === 'a') {
+                        return;
+                }
+
+                var imageSrc = $(this).attr('src');
                 var lastPeriod = imageSrc.lastIndexOf('.');
                 var fullresImageSrc = imageSrc.substr(0, lastPeriod) + '.fullres' + imageSrc.substr(lastPeriod);
-		$(this).wrap('<a href="' + fullresImageSrc + '" rel="darkbox"></a>');
-	});
+                $(this).wrap('<a href="' + fullresImageSrc + '" rel="darkbox"></a>');
+        });
 
-	$('a[rel=darkbox]').darkbox();
+        $('a[rel=darkbox]').darkbox();
 
-	// Highlight.js
-	hljs.initHighlightingOnLoad();
+        // Highlight.js
+        hljs.initHighlightingOnLoad();
 
-	// NProgress
-	NProgress.start();
+        // NProgress
+        NProgress.start();
 });
 
 $(window).load(function() {
-	// NProgress
-	NProgress.done();
+        // NProgress
+        NProgress.done();
 });
